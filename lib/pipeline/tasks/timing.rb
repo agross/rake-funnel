537beba28efc7546dcf5a3aece1b2366f7ef68a3
Alexander Groß <agross@therightstuff.de>
2014-03-21 00:18:26 +0100

    Extract refactoring timing task

--- lib/pipeline/tasks/timing.rb
+++ lib/pipeline/tasks/timing.rb
@@ -1,115 +1,18 @@
 require 'rake'
 require 'rake/tasklib'
-require 'smart_colored/extend'
+
+Dir["#{File.dirname(__FILE__)}/timing_support/*.rb"].each do |path|
+  require path
+end
 
 module Pipeline::Tasks
   class Timing < Rake::TaskLib
-    class Statistics
-      include Enumerable
-
-      attr_reader :started_at
-
-      def initialize
-        @stats = []
-        @started_at = Time.now
-      end
-
-      def each(&block)
-        @stats.each(&block)
-      end
-
-      def benchmark(task)
-        t0 = Time.now
-        begin
-          yield if block_given?
-        ensure
-          t1 = Time.now
-          @stats << { task: task, time: t1 - t0 }
-        end
-      end
-    end
-
-    class Report
-      class Column
-        attr_reader :header
-
-        def initialize(stats: [], header: '', accessor: -> (_) { '' })
-          @stats = stats
-          @header = header
-          @accessor = accessor
-        end
-
-        def width
-          longest_value = @stats.map { |s| @accessor.call(s) }.max_by { |m| m.length } || ''
-          width = longest_value.length
-          width = @header.length if width < @header.length
-          width
-        end
-
-        def format_header
-          @header.ljust(width)
-        end
-
-        def format_value(value)
-          @accessor.call(value).ljust(width)
-        end
-      end
-
-      SPACE = 3
-      HEADER_WIDTH = 70
-
-      def initialize(stats , opts = {})
-        @stats = stats
-        @opts = opts
-      end
-
-      def render
-        puts "\n\n" + '-' * HEADER_WIDTH
-        puts 'Build time report'
-        puts '-' * HEADER_WIDTH
-
-        puts columns.map { |c| c.format_header }.join(' ' * SPACE)
-        puts columns.map { |c| c.format_header.gsub(/./, '-') }.join(' ' * SPACE)
-
-        @stats.each do |timing|
-          puts columns.map { |c| c.format_value(timing) }.join(' ' * SPACE)
-        end
-
-        puts '-' * HEADER_WIDTH
-        puts 'Total'.ljust(columns[0].width) + ' ' * SPACE + format(Time.now - @stats.started_at)
-        puts status_message
-        puts '-' * HEADER_WIDTH
-      end
-
-      def columns
-        @columns ||= (
-          [
-            Column.new(stats: @stats, header: 'Target',   accessor: -> (timing) { timing[:task].name }),
-            Column.new(stats: @stats, header: 'Duration', accessor: -> (timing) { format(timing[:time]) })
-          ]
-        )
-      end
-
-      private
-      def format(seconds)
-        Time.at(seconds).utc.strftime('%H:%M:%S')
-      end
-
-      def status_message
-        status = @opts[:failed] ? 'Failed' : 'OK'
-        status = 'Status'.ljust(columns[0].width) + ' ' * SPACE + status
-
-        return status.bold.red if @opts[:failed]
-        status.bold.green
-      end
-    end
-
     attr_accessor :name
     attr_reader :stats
 
     def initialize(name = :timing)
       @name = name
-      @stats = Statistics.new
+      @stats = TimingSupport::Statistics.new
 
       yield self if block_given?
       define
@@ -147,7 +50,7 @@ module Pipeline::Tasks
       monkey_patch_rake_application
 
       task @name, :failed do |task, args|
-        Report.new(@stats, args).render
+        TimingSupport::Report.new(@stats, args).render
       end
 
       Rake.application.top_level_tasks.push(@name)
---
+++ lib/pipeline/tasks/timing_support/statistics.rb
@@ -0,0 +1,26 @@
+module Pipeline::Tasks::TimingSupport
+  class Statistics
+    include Enumerable
+
+    attr_reader :started_at
+
+    def initialize
+      @stats = []
+      @started_at = Time.now
+    end
+
+    def each(&block)
+      @stats.each(&block)
+    end
+
+    def benchmark(task)
+      t0 = Time.now
+      begin
+        yield if block_given?
+      ensure
+        t1 = Time.now
+        @stats << { task: task, time: t1 - t0 }
+      end
+    end
+  end
+end
