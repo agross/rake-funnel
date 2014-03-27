230d99517d76678277784962e9eed605b1c4c0be
Alexander Groß <agross@therightstuff.de>
2014-03-27 17:55:38 +0100

    Changing require order to fix tests on Linux

--- lib/pipeline/framework.rb
+++ lib/pipeline/framework.rb
@@ -5,6 +5,7 @@ module Pipeline::Tasks; end
 
 Dir[
   "#{File.dirname(__FILE__)}/*.rb",
+  "#{File.dirname(__FILE__)}/extensions/*.rb",
   "#{File.dirname(__FILE__)}/support/*.rb",
   "#{File.dirname(__FILE__)}/**/*.rb",
 ].each do |path|
