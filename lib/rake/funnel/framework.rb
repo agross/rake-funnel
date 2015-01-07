880cb28cd0c45fe33b29655673be3b9b4a25034e
Alexander Groß <agross@therightstuff.de>
2015-01-07 15:21:28 +0100

    Working around ordering issues of required files

--- lib/rake/funnel/framework.rb
+++ lib/rake/funnel/framework.rb
@@ -3,11 +3,13 @@ module Rake::Funnel::Integration; end
 module Rake::Funnel::Support; end
 module Rake::Funnel::Tasks; end
 
-Dir[
+[
   "#{File.dirname(__FILE__)}/*.rb",
   "#{File.dirname(__FILE__)}/extensions/*.rb",
   "#{File.dirname(__FILE__)}/support/*.rb",
   "#{File.dirname(__FILE__)}/*/*.rb",
 ].each do |path|
-  require path
+  Dir.glob(path).sort.each do |p|
+    require p
+  end
 end
