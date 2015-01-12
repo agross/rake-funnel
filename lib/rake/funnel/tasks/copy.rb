c42aebd9599cd604298591c26ad8c2510d7b5fca
Alexander Groß <agross@therightstuff.de>
2015-01-12 17:25:47 +0100

    Adding copy task

---
+++ lib/rake/funnel/tasks/copy.rb
@@ -0,0 +1,58 @@
+require 'rake/clean'
+require 'rake/tasklib'
+
+module Rake::Funnel::Tasks
+  class Copy < Rake::TaskLib
+    attr_accessor :name, :source, :target
+
+    def initialize(name = :copy)
+      @name = name
+
+      @source = []
+      @target = nil
+
+      yield self if block_given?
+      define
+    end
+
+    private
+    def define
+      target && CLEAN.include(target)
+
+      desc "Copy #{files.join(', ')} to #{target}"
+      task name do
+        raise 'Target not defined' unless target
+
+        files.each do |source|
+          next if File.directory?(source)
+
+          target = target_path(source)
+
+          dir = File.dirname(target)
+          RakeFileUtils.mkdir_p(dir) unless File.directory?(dir)
+
+          RakeFileUtils.cp(source, target)
+        end
+      end
+
+      self
+    end
+
+    def files
+      Rake::Funnel::Support::Finder.new(source, self, 'No files found.').all_or_default
+    end
+
+    def result_filename(template)
+      template.ext
+    end
+
+    def target_path(file)
+      target_relative = Pathname.new(file).relative_path_from(Pathname.new(common_path)).to_s
+      File.join(target, target_relative)
+    end
+
+    def common_path
+      @common_path ||= files.common_path
+    end
+  end
+end
---
+++ spec/rake/funnel/tasks/copy_spec.rb
@@ -0,0 +1,101 @@
+require 'rake'
+require 'rake/clean'
+require 'rake/funnel'
+require 'tmpdir'
+
+describe Rake::Funnel::Tasks::Copy do
+  before {
+    CLEAN.clear
+    Rake::Task.clear
+  }
+
+  describe 'defaults' do
+    its(:name) { should == :copy }
+    its(:source) { should eq([]) }
+    its(:target) { should be_nil }
+
+    it 'should not add the target file to the files to be cleaned' do
+      expect(CLEAN).to be_empty
+    end
+
+    describe 'overriding defaults' do
+      subject {
+        described_class.new do |t|
+          t.target = 'something'
+        end
+      }
+
+      it 'should add the target file to the files to be cleaned' do
+        expect(CLEAN).to include(subject.target)
+      end
+    end
+  end
+
+  describe 'execution' do
+    let(:source) { %w(bin/1 bin/2 bin/3/4 bin/directory/file bin/directory bin/directory-no-content) }
+    let(:target) { 'some path' }
+
+    subject! {
+      described_class.new do |t|
+        t.source = source
+        t.target = target
+      end
+    }
+
+    context 'failure' do
+      context 'target not defined' do
+        let(:target) { nil }
+
+        it 'should fail' do
+          expect(lambda { Rake::Task[subject.name].invoke }).to raise_error(/Target not defined/)
+        end
+      end
+    end
+
+    context 'success' do
+      let(:finder) { double(Rake::Funnel::Support::Finder).as_null_object }
+
+      before {
+        allow(finder).to receive(:all_or_default).and_return(source)
+        allow(Rake::Funnel::Support::Finder).to receive(:new).and_return(finder)
+
+        allow(File).to receive(:directory?).and_return(false)
+        source.last(2).each do |dir|
+          allow(File).to receive(:directory?).with(dir).and_return(true)
+        end
+
+        allow(RakeFileUtils).to receive(:mkdir_p)
+        allow(RakeFileUtils).to receive(:cp)
+      }
+
+      before {
+        Rake::Task[subject.name].invoke
+      }
+
+      def no_prefix(file)
+        file.sub(%r|bin/|, '')
+      end
+
+      it 'should create target directories' do
+        expect(RakeFileUtils).to have_received(:mkdir_p).with(subject.target + '/3')
+        expect(RakeFileUtils).to have_received(:mkdir_p).with(subject.target + '/directory')
+      end
+
+      it 'should skip source directories' do
+        source
+          .select { |src| File.directory?(src) }
+          .each do |src|
+          expect(RakeFileUtils).not_to have_received(:cp).with(src, anything)
+        end
+      end
+
+      it 'should copy files with common path removed' do
+        source
+          .select { |src| !File.directory?(src) }
+          .each do |src|
+          expect(RakeFileUtils).to have_received(:cp).with(src, File.join(subject.target, no_prefix(src)))
+        end
+      end
+    end
+  end
+end
