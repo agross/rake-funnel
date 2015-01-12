f321f603c879eb4ef56286202cfc5d6955c64646
Alexander Groß <agross@therightstuff.de>
2015-01-12 01:49:54 +0100

    Adding MSDeploy description

--- lib/rake/funnel/tasks/msdeploy.rb
+++ lib/rake/funnel/tasks/msdeploy.rb
@@ -26,6 +26,7 @@ module Rake::Funnel::Tasks
     def define
       CLEAN.include(log_file)
 
+      desc "Deploy #{deploy_source(args)}"
       task @name do
         mapper = Rake::Funnel::Support::Mapper.new(:MSDeploy)
         cmd = [quote(msdeploy), mapper.map(args)]
@@ -38,6 +39,14 @@ module Rake::Funnel::Tasks
       end
 
       self
+    end
+
+    def deploy_source(args)
+      source = (args || {}).fetch(:source, {})
+      path = source.first
+      return if path.nil?
+
+      Pathname.new(path[1]).relative_path_from(Pathname.new('.').realpath) rescue path[1]
     end
 
     def quote(value)
