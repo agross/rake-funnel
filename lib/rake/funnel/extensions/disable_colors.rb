f4187b1899981cab13731996c21617e87c187831
Alexander Groß <agross@therightstuff.de>
2015-03-07 13:34:17 +0100

    mod -> klass for module included parameter

--- lib/rake/funnel/extensions/disable_colors.rb
+++ lib/rake/funnel/extensions/disable_colors.rb
@@ -3,14 +3,14 @@ require 'smart_colored/extend'
 
 module Rake::Funnel::Extensions
   module DisableColors
-    def self.included(mod)
-      original_apply_format = mod.instance_method(:apply_format)
+    def self.included(klass)
+      original_apply_format = klass.instance_method(:apply_format)
 
       define_method(:apply_format) do |format|
         return self unless $stdout.tty?
 
         bind_to = self
-        bind_to = SmartColored::String.new(self) if mod == SmartColored::String
+        bind_to = SmartColored::String.new(self) if klass == SmartColored::String
         
         original_apply_format.bind(bind_to).call(format)
       end
