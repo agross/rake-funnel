341371624e9241d9d780878a71f057247c114560
Alexander Groß <agross@therightstuff.de>
2015-03-10 19:33:50 +0100

    Added version parser

---
+++ lib/rake/funnel/support/version.rb
@@ -0,0 +1,55 @@
+module Rake::Funnel::Support
+  class Version
+    class << self
+      def parse(context)
+        {
+          assembly_version: assembly_version(context),
+          assembly_file_version: assembly_file_version(context),
+          assembly_informational_version: assembly_informational_version(context)
+        }
+      end
+
+      private
+      def default_version(context)
+        context[:version] || '0'
+      end
+
+      def assembly_version(context)
+        strip_trailing_non_numeric(default_version(context))
+      end
+
+      def assembly_file_version(context)
+        numeric_build_number = strip_leading_non_numeric(context[:build_number])
+
+        [
+          assembly_version(context),
+          numeric_build_number
+        ].compact.join('.')
+      end
+
+      def assembly_informational_version(context)
+        build_number = context[:build_number]
+        join_using = '.'
+        join_using = '' if build_number =~ /^\D/
+
+        prefix = [default_version(context), build_number].compact.join(join_using)
+        sha = context[:sha]
+
+        [prefix, sha].compact.join('-')
+      end
+
+      def strip_trailing_non_numeric(str)
+        return nil if str.nil?
+        str.gsub(/[^\d\.].*/, '')
+      end
+
+      def strip_leading_non_numeric(str)
+        return nil if str.nil?
+        str = str.gsub(/[^\d\.]/, '')
+
+        return nil if str.empty?
+        str
+      end
+    end
+  end
+end
---
+++ spec/rake/funnel/support/version_spec.rb
@@ -0,0 +1,145 @@
+describe Rake::Funnel::Support::Version do
+  [
+    {
+      context: {
+        version: '1.2',
+        build_number: '42',
+        sha: 'sha'
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2.42',
+        assembly_informational_version: '1.2.42-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2-pre1',
+        build_number: '42',
+        sha: 'sha'
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2.42',
+        assembly_informational_version: '1.2-pre1.42-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2.3',
+        build_number: '42',
+        sha: 'sha' },
+      expected: {
+        assembly_version: '1.2.3',
+        assembly_file_version: '1.2.3.42',
+        assembly_informational_version: '1.2.3.42-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2.3-pre1',
+        build_number: '42',
+        sha: 'sha'
+      },
+      expected: {
+        assembly_version: '1.2.3',
+        assembly_file_version: '1.2.3.42',
+        assembly_informational_version: '1.2.3-pre1.42-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2',
+        build_number: '-pre',
+        sha: 'sha'
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2',
+        assembly_informational_version: '1.2-pre-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2',
+        build_number: '-pre42',
+        sha: 'sha'
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2.42',
+        assembly_informational_version: '1.2-pre42-sha'
+      }
+    },
+    {
+      context: {
+        version: '1.2',
+        build_number: nil,
+        sha: nil
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2',
+        assembly_informational_version: '1.2'
+      }
+    },
+    {
+      context: {
+        version: '1.2',
+        build_number: '42',
+        sha: nil
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2.42',
+        assembly_informational_version: '1.2.42'
+      }
+    },
+    {
+      context: {
+        version: '1.2'
+      },
+      expected: {
+        assembly_version: '1.2',
+        assembly_file_version: '1.2',
+        assembly_informational_version: '1.2'
+      }
+    },
+    {
+      context: {
+        version: nil,
+        build_number: nil,
+        sha: nil
+      },
+      expected: {
+        assembly_version: '0',
+        assembly_file_version: '0',
+        assembly_informational_version: '0'
+      }
+    },
+    {
+      context: { },
+      expected: {
+        assembly_version: '0',
+        assembly_file_version: '0',
+        assembly_informational_version: '0'
+      }
+    }
+  ].each do |spec|
+    context "version #{spec[:context][:version] || 'none'}, build number #{spec[:context][:build_number] || 'none'}, SHA #{spec[:context][:sha] || 'none'}" do
+      let(:parsed) { described_class.parse(spec[:context]) }
+
+      it "should generate assembly version #{spec[:expected][:assembly_version]}" do
+        expect(parsed[:assembly_version]).to eq(spec[:expected][:assembly_version])
+      end
+
+      it "should generate assembly file version #{spec[:expected][:assembly_file_version]}" do
+        expect(parsed[:assembly_file_version]).to eq(spec[:expected][:assembly_file_version])
+      end
+
+      it "should generate assembly informational version #{spec[:expected][:assembly_informational_version]}" do
+        expect(parsed[:assembly_informational_version]).to eq(spec[:expected][:assembly_informational_version])
+      end
+    end
+  end
+end
