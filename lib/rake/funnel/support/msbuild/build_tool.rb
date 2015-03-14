1f80095433f4451963c465b31f6bc5667885c70d
Alexander Groß <agross@therightstuff.de>
2015-03-14 13:11:02 +0100

    Improved build tool detection a bit

--- lib/rake/funnel/support/msbuild/build_tool.rb
+++ lib/rake/funnel/support/msbuild/build_tool.rb
@@ -2,26 +2,22 @@ module Rake::Funnel::Support::MSBuild
   class BuildTool
     class << self
       def find
-        if Rake::Win32.windows?
-          require 'win32/registry'
+        return 'xbuild' unless Rake::Win32.windows?
 
-          %w{12.0 4.0 3.5 2.0}.collect { |version|
-            msbuild = nil
-            key = "SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\#{version}"
+        require 'win32/registry'
 
-            begin
-              Win32::Registry::HKEY_LOCAL_MACHINE.open(key) do |reg|
-                candidate = "#{reg['MSBuildToolsPath']}\\msbuild.exe"
-                msbuild = candidate if File.exists?(candidate)
-              end
-            rescue
+        %w(12.0 4.0 3.5 2.0).collect { |version|
+          key = "SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions\\#{version}"
+
+          begin
+            Win32::Registry::HKEY_LOCAL_MACHINE.open(key) do |reg|
+              candidate = File.join(reg['MSBuildToolsPath'] || '', 'msbuild.exe')
+              next candidate if File.exists?(candidate)
             end
-
-            msbuild
-          }.compact.first
-        else
-          'xbuild'
-        end
+          rescue
+            next
+          end
+        }.compact.first
       end
     end
   end
