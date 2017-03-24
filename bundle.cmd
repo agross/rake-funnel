5c368ef135677c9ddc19bf5c104f5d352e099b38
Alexander Groß <agross@therightstuff.de>
2017-03-24 09:37:55 +0100

    Update build helper scripts for Windows

--- bundle.cmd
+++ bundle.cmd
@@ -1,13 +1,20 @@
 @echo off
 setlocal
 
-set LANG=en_US.UTF-8
+pushd "%~dp0"
 
-call gem.bat which bundler > NUL 2>&1
+chcp 65001 > NUL
+
+if defined TEAMCITY_PROJECT_NAME echo ##teamcity[blockOpened name='%0 %*']
+
+call gem which bundler > NUL 2>&1
 if errorlevel 1 (
   echo Installing bundler...
-  call gem.bat install bundler --no-ri --no-rdoc
+  call gem install bundler --no-ri --no-rdoc
 )
 
 call bundle.bat %*
+
+:exit
+if defined TEAMCITY_PROJECT_NAME echo ##teamcity[blockClosed name='%0 %*']
 exit /b %errorlevel%
